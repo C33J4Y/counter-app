7045315545229409ed82aea19d48de89d11d64eb
C33J4Y <61567416+C33J4Y@users.noreply.github.com>
2021-03-11 09:01:37 -0500

    Added max capacity message for count

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import {useState} from 'react';
 import './App.css';
 
+function MaxCapacity(){
+  return <h1>Store at Max Capacity</h1>;     
+}
+
 function App() {
   const [count, setCount] = useState(0);
-  return (
-    <div>
-      <h1>People Counter</h1>
-      <button onClick={() => setCount(count + 1)}>Increase</button>
-      <p>{count}</p>
-      <button onClick={count > 0 ? () => setCount(count - 1) : () => setCount(0)}>Decrease</button>
-    </div>
-  );
+
+  if(count >= 10){
+    return (
+      <div>
+        <MaxCapacity/>
+        <button onClick={count > 0 ? () => setCount(count - 1) : () => setCount (0)}>Decrease</button>
+        </div>  
+      
+    );
+  }
+
+  if(count < 10){
+    return (
+      <div>
+        <h1>People Counter</h1>
+        <button onClick={() => setCount(count + 1)}>Increase</button>
+        <p>{count}</p>
+        <button onClick={count > 0 ? () => setCount(count - 1) : () => setCount(0)}>Decrease</button>
+      </div>
+    );
+  }
 }
 
 export default App;
